acb4fbfd023685cbd59db72f28f96d437c7de38c
Ayan <ahmad.ayan2012@gmail.com>
2026-01-05 23:22:28 +0530

    second commit

--- main.py
+++ main.py
@@ -7,6 +7,24 @@ INT = INTEGER
 IDENTIFIER = "IDENTIFIER"
 ASSIGN = "ASSIGN"
 NEWLINE = "NEWLINE"
+EOF = "EOF"
+DIVIDE = "DIVIDE"
+DIV = DIVIDE
+MULTIPLY = "MULTIPLY"
+MUL = MULTIPLY
+POWER = "POWER"
+POW = POWER
+SUBTRACT = "SUBTRACT"
+SUB = SUBTRACT
+
+PRECEDENCE = {
+    ADD:1,
+    SUB:1,
+    MUL:2,
+    DIV:2,
+    POW:3
+}
+
 
 class Token():
     def __init__(self, cat:str, value:Any):
@@ -21,7 +39,6 @@ class Tokenizer():
     def __init__(self, string: str):
         self.string:str = string
         self.current_idx:int = 0
-        self.current_token:Token | None = self.get_next_token() 
     def get_current_char(self) -> str | None:
         if self.current_idx >= len(self.string):
             return None
@@ -34,17 +51,21 @@ class Tokenizer():
                 self.current_idx += 1
             else:
                 break
-
+    def peek(self):
+        if self.current_idx + 1 < len(self.string):
+            return self.string[self.current_idx+1]
+        else:
+            return None
     def get_next_token(self):
         self.skip_whitespace()
 
         current_char = self.get_current_char()
         if current_char is None:
-            return None  # End of input
+            return Token(EOF, None)  # End of input
 
         if current_char.isdigit():
             value = ""
-            while self.get_current_char() is not None and self.get_current_char().isdigit():
+            while self.get_current_char() is not None and self.get_current_char().isdigit(): #pyright: ignore[reportOptionalMemberAccess]
                 value += self.get_current_char() # pyright: ignore[reportOperatorIssue]
                 self.current_idx += 1
             return Token(INT, int(value))
@@ -55,6 +76,15 @@ class Tokenizer():
         elif current_char == "=":
             self.current_idx+=1
             return Token(ASSIGN, None)
+        elif current_char == "-":
+            self.current_idx+=1
+            return Token(SUB, None)
+        elif current_char == "*" and self.peek()=="*":
+            self.current_idx+=1
+            return Token(POW, None)
+        elif current_char == "*":
+            self.current_idx+=1
+            return Token(MUL, None)
         elif current_char == "\n":
             self.current_idx+=1
             return Token(NEWLINE, None)
@@ -68,20 +98,32 @@ class Tokenizer():
 
         raise SyntaxError(f'Unexpected token "{current_char}"')
     
-    def eat(self, expected_type:str):
-        if self.current_token is None:
-            raise SyntaxError('Unexpected end of input')
-        if self.current_token.type != expected_type:
-            raise SyntaxError(f'Expected {expected_type}, got {self.current_token.type} ({self.current_token.value})')
-        self.current_token = self.get_next_token()
-    def get_all_tokens(self) -> list[Token]:
-        tkns=[]
-        while self.current_token is not None:
-            tkns.append(self.current_token)
-            self.current_token = self.get_next_token()
-        return tkns
+def infix_to_rpn(tokens: list[Token]) -> list[Token]:
+    output: list[Token] = []
+    operators: list[Token] = []
 
-def expr(tokens: list[Token | None]):
+    for token in tokens:
+        if token.type == INT or token.type == IDENTIFIER:
+            output.append(token)
+
+        elif token.type == ADD:
+            while (
+                operators
+                and operators[-1].type == ADD
+                and PRECEDENCE[operators[-1].type] >= PRECEDENCE[token.type]
+            ):
+                output.append(operators.pop())
+            operators.append(token)
+
+        else:
+            raise SyntaxError(f"Unsupported token {token.type}")
+
+    while operators:
+        output.append(operators.pop())
+
+    return output
+
+def eval_rpm(tokens: list[Token | None]):
     stack = []
     for token in tokens:
         if token is None:
@@ -91,18 +133,40 @@ def expr(tokens: list[Token | None]):
         elif token.type == ADD:
             rhs = stack.pop()
             lhs = stack.pop()
-            stack.append(rhs + lhs)
+            stack.append(lhs + rhs)
+        elif token.type == SUB:
+            rhs = stack.pop()
+            lhs = stack.pop()
+            stack.append(lhs - rhs)
+        elif token.type == MUL:
+            rhs = stack.pop()
+            lhs = stack.pop()
+            stack.append(lhs * rhs)
+        elif token.type == DIV:
+            rhs = stack.pop()
+            lhs = stack.pop()
+            stack.append(lhs / rhs)
         else:
             raise SyntaxError(f'Unexpected token {token.type}. Expected INT or ADD')
     return stack[0]
 
+class Parser:
+    def __init__(self, tokenizer: Tokenizer):
+        self.tokenizer = tokenizer
+        self.current_token = self.tokenizer.get_next_token()
+
+    def eat(self, expected_type: str):
+        if self.current_token.type != expected_type:
+            raise SyntaxError(
+                f"Expected {expected_type}, got {self.current_token.type}"
+            )
+        self.current_token = self.tokenizer.get_next_token()
+
+
+
+
 
 def main():
-    tknr = Tokenizer("hello=4 5+")
-    tkns=tknr.get_all_tokens()
-    i=0
-    #print(tknr.current_token)
-    #print(tknr.get_all_tokens())
-    #print(expr(tknr.get_all_tokens()))
+    pass
 if __name__ == "__main__":
     main()
