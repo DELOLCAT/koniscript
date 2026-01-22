6b667516a11312ae638ea9973dc03de0e628b681
Ayan <ahmad.ayan2012@gmail.com>
2026-01-22 16:20:34 +0000

    really stupid fix really

--- build.py
+++ build.py
@@ -37,7 +37,7 @@ def main():
             print(f"❌ Build failed: {e}")
             return
 
-    print("[green b u]✅ Build completed! Result in dist/")
+    print("[green b u]Build completed! Result in dist/")
 
 if __name__ == "__main__":
     main()
