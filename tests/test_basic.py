a515698c2df5773af86956d87d0c1786d2a9aa06
Ayan <ahmad.ayan2012@gmail.com>
2026-03-02 10:51:38 +0000

    add tests and fix tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -4,3 +4,5 @@ def test_print():
     assert omni.run(Path(__file__).parent / 'test_print.om').stdout == b'Hello, world!\n'
 def test_recurse():
     assert omni.run(Path(__file__).parent.parent / 'examples' / 'fib.om').stdout == b'55\n'
+def test_funcs():
+    assert omni.run(Path(__file__).parent / 'test_funcs.om').stdout == b'hihihihihi\nhihihihihi\nhihihihihihihihihihi\n'
